b18e9bef7ae3ce07c9efeb354f7075a7ebc27653
贾国强 <jgq@bytedance.com>
2023-12-15 13:16:58 +0800

    第二次提交

--- functions/nodejs/test1/index.js
+++ functions/nodejs/test1/index.js
@@ -15,4 +15,7 @@ module.exports = async function (params, context, logger) {
   // 在这里补充业务代码
   logger.info("hhhh")
   logger.info("hhhh")
+  logger.info("AAAA")
+  logger.info("AAAB")
+  logger.info("CCCC")
 }
